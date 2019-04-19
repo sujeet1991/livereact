f8a912854bf9fdd1d25b97005c2636b12d066a7c
sujeet <yadavsujeet295@gmail.com>
2019-04-20 00:59:56 +0530

    tracker

--- src/component/Projectmng/Projecttracker.js
+++ src/component/Projectmng/Projecttracker.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import input from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
 export default class Projecttracker extends Component {
     constructor(props){
         super(props);
         this.state={
             jobStatus:[],
-            projectTrack:{},
+            projecttrack:{},
+            projectdetail:{}
 
         }
     }
     componentWillMount(){
-      console.log('mount');
+      //console.log('mount');
         this.jobStatus();
         var projectdetaildata =localStorage.getItem("projectDetail");
-      
+        var gettrackerdetailold=localStorage.getItem("getproject")
+        //console.log(gettrackerdetailold)
+        let gettrackerdetail=JSON.parse(gettrackerdetailold);
+
+        let gettrackprojectjson={
+          "appointmentDate":gettrackerdetail.appointmentDate,
+          "lostDate":gettrackerdetail.lostDate,
+          "projectStartDate":gettrackerdetail.projectStartDate,
+          "projectHoldDate":gettrackerdetail.projectHoldDate,
+          "targetDate":gettrackerdetail.targetDate,
+          "actualCompletionDate":gettrackerdetail.actualCompletionDate,
+        } 
+        //console.log(gettrackprojectjson)
+        this.setState({projectdetail:projectdetaildata,projecttrack:gettrackprojectjson}) 
       
         //this.setState({projectTrack:projectdetaildata})
-       console.log("local"+projectdetaildata);
+       
     }
     jobStatus() {
         let statethis=this;
@@ -36,18 +50,43 @@ export default class Projecttracker extends Component {
        })
     }
 
-    componentWillReceiveProps(newProps){
-        console.log("hi");
-        console.log("sujeet"+newProps);
+    tarckonChange=(fname,e)=>{
+      console.log(fname);
+      console.log(e.target.value);
+      let projecttrack=this.state.projecttrack;
+      if(fname==='appointmentDate' ||fname==='lostDate'||fname==='projectStartDate'||fname==='projectHoldDate'||fname==='targetDate'||fname==='actualCompletionDate'){
+        projecttrack[fname]=e.target.value+" "+"00:00:00";
+     }else{
+        projecttrack[fname]=e.target.value;
+     }
+      this.setState({
+        projecttrack
+      })
+
     }
+    savedata=()=>{
+      let projectdetail =this.state.projectdetail;
+      let projecttrack=this.state.projecttrack;
+      console.log(projectdetail)
+      console.log(projecttrack)
+      
+      
+
+
+
+      
+      //console.log(Object.assign(projectdetail,projecttrack))
+    }
+    
 
   render() {
-    //console.log(this.state.projectTrack)
-      //console.log(this.props)
+   
+      console.log(this.state.projecttrack)
       let appointmentDatenew,lostDatenew,projectStartDatenew,projectHoldDatenew,targetDatenew,actualCompletionDatenew=null;
      
-      if(this.props.projecttrack.length!==0){
-        let {appointmentDate,lostDate,projectStartDate,projectHoldDate,targetDate,actualCompletionDate}= this.props.projecttrack;
+      if(this.state.projecttrack){
+       
+        let {appointmentDate,lostDate,projectStartDate,projectHoldDate,targetDate,actualCompletionDate}= this.state.projecttrack;
         
         appointmentDatenew=appointmentDate.split(" ")[0];
         lostDatenew=lostDate.split(" ")[0];
@@ -69,7 +108,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" name="appointmentDate" ref="appointmentDate"  value={appointmentDatenew||''} className="form-control pull-right" id=""/>
+            <input type="date" name="appointmentDate" onChange={(e)=>this.tarckonChange('appointmentDate',e)} ref="appointmentDate"  value={appointmentDatenew||''} className="form-control pull-right" id=""/>
           </div>
           
         </div>
@@ -81,7 +120,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" value={lostDatenew||''} name="lostDate" ref="lostDate" className="form-control pull-right" id=""/>
+            <input type="date" value={lostDatenew||''} name="lostDate" ref="lostDate" className="form-control pull-right" onChange={(e)=>this.tarckonChange('lostDate',e)} id=""/>
           </div>
          
         </div>
@@ -93,7 +132,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" name="projectStartDate" ref="projectStartDate" value={projectStartDatenew||''} className="form-control pull-right" id=""/>
+            <input type="date" name="projectStartDate" onChange={(e)=>this.tarckonChange('projectStartDate',e)} ref="projectStartDate" value={projectStartDatenew||''} className="form-control pull-right" id=""/>
           </div>
          
         </div>
@@ -105,7 +144,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" name="projectHoldDate" ref="projectHoldDate" value={projectHoldDatenew||''}  className="form-control pull-right" id="datepicker_hold"/>
+            <input type="date" name="projectHoldDate" onChange={(e)=>this.tarckonChange('projectHoldDate',e)} ref="projectHoldDate" value={projectHoldDatenew||''}  className="form-control pull-right" id=""/>
           </div>
          
         </div>
@@ -113,7 +152,7 @@ export default class Projecttracker extends Component {
       <div className="col-sm-4">
         <div className="form-group">
           <label>Completion Target (in Days)</label> 
-          <input type="text" name="completionTarget" ref="completionTarget" value={this.props.projecttrack.completionTarget||''} className="form-control"/>
+          <input type="text" name="completionTarget" ref="completionTarget" value={this.state.projecttrack.completionTarget||''}  onChange={(e)=>this.tarckonChange('completionTarget',e)} className="form-control"/>
         </div>
       </div>
       <div className="col-sm-4">
@@ -123,7 +162,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" name="targetDate" ref="targetDate"  value={targetDatenew||''} className="form-control pull-right" id=""/>
+            <input type="date" name="targetDate" ref="targetDate" onChange={(e)=>this.tarckonChange('targetDate',e)}  value={targetDatenew||''} className="form-control pull-right" id=""/>
           </div>
          
         </div>
@@ -135,7 +174,7 @@ export default class Projecttracker extends Component {
             <div className="input-group-addon">
               <i className="fa fa-calendar"></i>
             </div>
-            <DatePicker type="text" name="actualCompletionDate" ref="actualCompletionDat" value={actualCompletionDatenew||''} className="form-control pull-right" id=""/>
+            <input type="date" name="actualCompletionDate"  ref="actualCompletionDat" value={actualCompletionDatenew||''}  onChange={(e)=>this.tarckonChange('actualCompletionDate',e)} className="form-control pull-right" id=""/>
           </div>
           
         </div>
@@ -143,12 +182,12 @@ export default class Projecttracker extends Component {
       <div className="col-sm-4">
         <div className="form-group">
           <label>Job Status</label> 
-          <select name="jobStatus" ref="jobStatus" className="form-control">
+          <select name="jobStatus" ref="jobStatus" className="form-control" onChange={(e)=>this.tarckonChange('jobStatus',e)}>
           {this.state.jobStatus==='Invalid tenant, please contact system admin'?
                             <option>No Data found</option>:
                             this.state.jobStatus.map((curr,index)=>{
                                 let highlight=null;
-                                if(curr.id===this.props.projecttrack.jobStatus){
+                                if(curr.id===this.state.projecttrack.jobStatus){
                                     highlight='selected'
                                 }
                                
@@ -164,20 +203,20 @@ export default class Projecttracker extends Component {
       <div className="col-sm-4">
         <div className="form-group">
           <label>Awarded Project Value</label> 
-          <input type="text" name="awardedProjectValue" ref="awardedProjectValue"  value={this.props.projecttrack.awardedProjectValue||''} className="form-control"/>
+          <input type="text" name="awardedProjectValue" onChange={(e)=>this.tarckonChange('awardedProjectValue',e)} ref="awardedProjectValue"  value={this.state.projecttrack.awardedProjectValue||''} className="form-control"/>
         </div>
       </div>
       <div className="col-sm-4">
         <div className="form-group">
           <label>Design Value </label> 
-          <input type="text" name="awardedDesignValue" ref="awardedDesignValue" value={this.props.projecttrack.awardedDesignValue||''} className="form-control"/>
+          <input type="text"  onChange={(e)=>this.tarckonChange('awardedDesignValue',e)} name="awardedDesignValue" ref="awardedDesignValue" value={this.state.projecttrack.awardedDesignValue||''} className="form-control"/>
         </div>
       </div>
     </div>
     <div className="row mgtop">
       <div className="col-sm-12">
         <div className="form-group">
-          <button type="button" onClick={(e)=>this.props.tabchange(e,'Site Details')} className="btn btn-default fix-button" style={{marginRight:'8px'}}>Continue</button>
+          <button type="button" onClick={(e)=>this.savedata(e)} className="btn btn-default fix-button" style={{marginRight:'8px'}}>Continue</button>
           <button type="button" className="btn btn-default fix-button clr-new">Close</button>
         </div>
       </div>
