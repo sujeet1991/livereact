cd40d75005427ac69da74509fae679c8406da4e9
sujeet <yadavsujeet295@gmail.com>
2019-04-24 20:08:37 +0530

    team validation

--- src/component/Projectmng/teamstature.js
+++ src/component/Projectmng/teamstature.js
@@ -9,7 +9,7 @@ class Teamstruture extends Component{
         this.state={
             show:false,
             teamrecord:[],
-            teamuser:{"teamId":"","projectId":geturl[getnumber],"employeeId":"","roleId":"","startDate":"","endDate":"","allocatedHours":"","createdBy":"2019-04-23 00:00:00","modifiedBy":"2019-04-23 00:00:00"},
+            teamuser:{"teamId":"","projectId":geturl[getnumber],"employeeId":"","roleId":"","startDate":"","endDate":"","allocatedHours":0,"createdBy":"2019-04-23 00:00:00","modifiedBy":"2019-04-23 00:00:00"},
             msgsuccess:null,
             getrollData:[],
             getempName:[],
@@ -23,7 +23,7 @@ class Teamstruture extends Component{
       var teamdata= localStorage.getItem('getproject');
       
       let teamdisplay= JSON.parse(teamdata);
-      console.log(teamdisplay.projectCode)
+      //console.log(teamdisplay.projectCode)
       this.setState({
         teamrecord:teamdisplay.team,
         getadditionalData:{projectName:teamdisplay.projectName,projectCode:teamdisplay.projectCode}
@@ -73,11 +73,39 @@ class Teamstruture extends Component{
   }
 
 
-    handleshow=(data,e)=>{
-       
-        this.setState({
-            show:true,
+    handleshow=(dataid,e)=>{
+        var geturl =window.location.href.split('/');
+        let getnumber=geturl.length-1;
+        if(dataid!==''){
+           
+            var datafilter=this.state.teamrecord;
+            let activefiletr=datafilter.filter(function(recordid){
+                //console.log("all"+recordid);
+                if(recordid.teamId===dataid){
+                    return recordid;
+                }
             })
+            let dataset=activefiletr[0];
+            //console.log(dataset.allocatedHours)
+            let setdatastate={"teamId":dataset.teamId,"projectId":geturl[getnumber],"employeeId":dataset.employeeId,"roleId":dataset.roleId,"startDate":dataset.startDate,"endDate":dataset.endDate,"allocatedHours":dataset.allocatedHours,"createdBy":"2019-04-23 00:00:00","modifiedBy":"2019-04-23 00:00:00"}
+
+            //console.log(setdatastate);
+            this.setState({
+                show:true,
+                teamuser:setdatastate
+            });
+        }else{
+            let setdatastate={"teamId":"","projectId":geturl[getnumber],"employeeId":"","roleId":"","startDate":"","endDate":"","allocatedHours":"","createdBy":"2019-04-23 00:00:00","modifiedBy":"2019-04-23 00:00:00"}
+            this.setState({
+                show:true,
+                teamuser:setdatastate
+                
+            });
+        }
+       
+       
+       
+        
 
     }
     
@@ -112,15 +140,29 @@ teamValidation(){
         errors['roleId']="Select Role ";
 
     }
-   
-     if(teamdata.startDate==""){
+    //console.log("ST DT: "+teamdata.startDate);
+    //console.log("END DT: "+teamdata.endDate);
+    var strdata= ""+teamdata.startDate;
+    var enddata= ""+teamdata.endDate;
+    
+    //console.log("==="+strdata);
+    //console.log("end==="+enddata);
+    if(enddata < strdata){
+        isValid=false;
+        errors['endDate']="Please change  End date ";
+    }
+   //console.log("StartDate:  "+teamdata.startDate);
+     if(teamdata.startDate==""||teamdata.startDate==null){
+         //console.log("Inside if empty");
         isValid=false;
         errors['startDate']="Please select Start date ";
     }
-     if(teamdata.endDate==""){
+     if(teamdata.endDate==""||teamdata.endDate==null){
+
         isValid=false;
         errors['endDate']="Please select End date ";
     }
+    
      if(teamdata.endDate===""){
         isValid=false;
         errors['endDate']="Please select End date ";
@@ -128,6 +170,10 @@ teamValidation(){
     if(teamdata.allocatedHours===""){
         isValid=false;
         errors['allocatedHours']="Please enter allocatedHours ";
+    } 
+    if(typeof (this.refs.allocatedHours.value)!== 'number'){
+        isValid=false;
+        errors['allocatedHours']="Please enter Number ";
     }
     this.setState({errors: errors});
     return isValid;
@@ -136,7 +182,7 @@ teamValidation(){
 
     saveteamData=(e)=>{
         let teamdata= this.state.teamuser;
-        console.log("ss"+teamdata)
+        //console.log(teamdata)
         var CheckValid=this.teamValidation();
         if(CheckValid){
             (async () => {
@@ -168,12 +214,24 @@ teamValidation(){
         }
 
     render(){
-    console.log(this.state.teamuser)
+    //console.log(this.state.teamuser)
     var errorstyle = {
         color: 'red',
         position:'absolute',
         fontSize:'12px',
     };
+    var startDatenew,endDatenew=null;
+    let {startDate,endDate}=this.state.teamuser;
+    if(startDate!==""||startDate!==null){
+        var datesplite=startDate.split(" ");
+        startDatenew=datesplite[0]
+
+    }
+    if(endDate!==""||endDate!==null){
+        var datesplite=endDate.split(" ");
+        endDatenew=datesplite[0]
+    }
+    
     
         
         return(
@@ -214,7 +272,7 @@ teamValidation(){
                     </div>
                 </div>
 
-               {this.state.show===true? <Modelteam  title="Edit / Add Resources To Project" closemodel={this.handlehide}>
+               {this.state.show===true? <Modelteam removeButton="hide"  title="Edit / Add Resources To Project" closemodel={this.handlehide}>
                <div className="row">
                 <div className="col-sm-4">
                     <div className="form-group">
@@ -248,11 +306,11 @@ teamValidation(){
                 <div className="col-sm-4">
                     <div className="form-group">
                     <label>Role in Project </label>
-                    <select  ref="roleId" name="roleId" onChange={(e)=>this.teamonChange('roleId',e)} className="form-control">
+                    <select  ref="roleId"  value={this.state.teamuser.roleId||""} name="roleId" onChange={(e)=>this.teamonChange('roleId',e)} className="form-control">
                     <option value="">--- Select ---</option>
                         {this.state.getrollData.length!==0? 
                             this.state.getrollData.map((curr,index)=>{
-                                console.log(curr)
+                                //console.log(curr)
                                 return(
                                     <option value={curr.id} key={index}>{curr.paramValue}</option>
                                 )
@@ -272,9 +330,10 @@ teamValidation(){
                         <div className="input-group-addon">
                         <i className="fa fa-calendar"></i>
                         </div>
-                        <input type="date" name="startDate" onChange={(e)=>this.teamonChange('startDate',e)} ref="startDate" className="form-control pull-right" id=""/>
-                        <span style={errorstyle}>{this.state.errors["startDate"]}</span>
+                        <input type="date" name="startDate" value={startDatenew ||""} onChange={(e)=>this.teamonChange('startDate',e)} ref="startDate" className="form-control pull-right" id=""/>
+                        
                     </div>
+                        <span style={errorstyle}>{this.state.errors["startDate"]}</span>
                     
                     </div>
                 </div>
@@ -285,16 +344,17 @@ teamValidation(){
                         <div className="input-group-addon">
                         <i className="fa fa-calendar"></i>
                         </div>
-                        <input type="date" name="endDate" onChange={(e)=>this.teamonChange('endDate',e)}  ref="endDate" className="form-control pull-right" id=""/>
-                        <span style={errorstyle}>{this.state.errors["endDate"]}</span>
+                        <input type="date" name="endDate" onChange={(e)=>this.teamonChange('endDate',e)}  ref="endDate" value={endDatenew||""} className="form-control pull-right" id=""/>
+                        
                     </div>
+                    <span style={errorstyle}>{this.state.errors["endDate"]}</span>
                     
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="form-group">
                     <label>Allocated Hour</label>
-                    <input type="text" className="form-control" onChange={(e)=>this.teamonChange('allocatedHours',e)} ref="allocatedHours" name="allocatedHours"/> 
+                    <input type="text" className="form-control" onChange={(e)=>this.teamonChange('allocatedHours',e)} ref="allocatedHours" value={this.state.teamuser.allocatedHours||""} name="allocatedHours"/> 
                     <span style={errorstyle}>{this.state.errors["allocatedHours"]}</span>
                     </div>
                 </div>
